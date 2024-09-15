3b730b75e1930850ac749e9776dab4caea7afe3f
Emmett McDow <emmett.mcdow@gmail.com>
2024-09-15 15:28:51 -0700

    Migrated to typescript (stop doing this lol)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
+import './App.css';
 import placeholderSquare from "./placeholder-square.png";
-import "./App.css";
 
 function SongInfo() {
   let artist = "Honestus";
@@ -9,22 +10,21 @@ function SongInfo() {
   return (
     <div id="song-info">
       <h1 id="artist-header">{artist}</h1>
-      <img width="300" height="300" src={art} />
+      <img width="300" height="300" alt="album-art" src={art} />
       <h2 id="song-or-album-title">{song}</h2>
     </div>
   );
 }
 function ButtonBox() {
   let providers = ["spotify", "applemusic", "youtube"];
-  let buttons = providers.map((provider) => {
-    return <IconLink provider={provider} />;
-  });
+  let buttons = providers.map(provider => <IconLink provider={provider} /> );
   return <div id="button-box">{buttons}</div>;
 }
-function IconLink(provider) {
+function IconLink({ provider }: { provider: string }) {
+  let alt = provider + "-icon";
   return (
     <a href={provider}>
-      <img width="100" height="100" src={placeholderSquare} />
+      <img width="100" height="100" alt={alt} src={placeholderSquare} />
     </a>
   );
 }
