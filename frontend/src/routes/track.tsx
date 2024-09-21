0a2662380f20ce4e0056f4f84ba72ac96b0a230a
Emmett McDow <emmett.mcdow@gmail.com>
2024-09-20 21:31:00 -0700

    "styled" track page

--- frontend/src/routes/track.tsx
+++ frontend/src/routes/track.tsx
@@ -7,24 +7,24 @@ function SongInfo() {
   let art = placeholderSquare;
 
   return (
-    <div id="song-info">
-      <h1 id="artist-header">{artist}</h1>
-      <img width="300" height="300" alt="album-art" src={art} />
-      <h2 id="song-or-album-title">{song}</h2>
+    <div className="my-5">
+      <h1 className="text-4xl">{artist}</h1>
+      <img width="250" height="250" alt="album-art" src={art} className="mx-auto my-2"/>
+      <h2 className="text-2xl">{song}</h2>
     </div>
   );
 }
 function ButtonBox({togglePrompt}: {togglePrompt: Function}) {
   let providers = ["spotify", "applemusic", "youtube"];
   let buttons = providers.map(provider => <IconLink provider={provider} key={provider} togglePrompt={togglePrompt}/> );
-  return <div id="button-box">{buttons}</div>;
+  return <div id="button-box" className="flex flex-wrap items-center justify-center mx-auto w-full h-full p-10" >{buttons}</div>;
 }
 
 function IconLink({ provider, togglePrompt }: { provider: string, togglePrompt: Function}) {
   let alt = provider + "-icon";
   return (
-    <button onClick={(_) => {togglePrompt(true); console.log("prompt toggled");} }>
-      <img width="100" height="100" alt={alt} src={placeholderSquare} />
+    <button className="mx-5 w-1/5" onClick={(_) => {togglePrompt(true); console.log("prompt toggled");} }>
+      <img  alt={alt} src={placeholderSquare} />
     </button>
   );
 }
@@ -33,11 +33,13 @@ function SubscriptionPrompt({visible, toggle}: {visible: boolean, toggle: Functi
   let artist = "Honestus";
   if (visible) {
     return (
-      <div id="subscription-prompt">
+      <div className="z-50 absolute w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-indigo-200 p-5">
         <p>wanna be notified when {artist} drops? (it's free)</p>
-        <input name="email"/>
-        <button onClick={(_) => toggle(false)}>yes</button>
-        <button onClick={(_) => toggle(false)}>just take me to the tunes</button>
+        <form className="my-2">
+          <input className="w-1/2" name="email"/>
+          <button onClick={(_) => toggle(false)} className="mx-2 bg-emerald-500 rounded-lg"><span className="p-4 py-2 text-white">yes</span></button>
+        </form>
+        <button onClick={(_) => toggle(false)} className="mx-2 bg-indigo-500 rounded-lg"><span className="p-4 py-2 text-white">i just wanna rock(no)</span></button>
       </div>
     );
   } else {
@@ -48,10 +50,10 @@ function SubscriptionPrompt({visible, toggle}: {visible: boolean, toggle: Functi
 export default function TrackPage() {
   const [visibleSubsPrompt, togglePrompt] = useState(false);
   return (
-      <>
+      <div className="h-screen flex flex-col content-center text-center p-5">
         <SongInfo />
         <ButtonBox togglePrompt={togglePrompt}/>
         <SubscriptionPrompt visible={visibleSubsPrompt} toggle={togglePrompt}/>
-      </>
+      </div>
   );
 }
