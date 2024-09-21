cd46d019e0979b8e19ac3e32379cf70c78999e11
Emmett McDow <emmett.mcdow@gmail.com>
2024-09-21 09:39:23 -0700

    "Styled" sign-up

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -3,8 +3,7 @@ import {eyeOff} from 'react-icons-kit/feather/eyeOff';
 import {eye} from 'react-icons-kit/feather/eye';
 import { useState } from 'react';
 
-export default function Login() {
-  const [password, setPassword] = useState("");
+export function Password({name, password, setPassword}: {name: string, password: string, setPassword: Function}) {
   const [type, setType] = useState('password');
   const [icon, setIcon] = useState(eyeOff);
   const handleToggle = () => {
@@ -16,18 +15,27 @@ export default function Login() {
       setType('password')
    }
   }
+
+  return (
+    <div className="flex mb-2">
+      {/* TODO: why tf is the password in the url */}
+      <input className="w-full rounded-lg p-1" type={type} name={name} placeholder={name} value={password} onChange={(e) => setPassword(e.target.value)}/>
+      <span className="flex justify-around items-center" onClick={handleToggle}>
+        <Icon className="absolute mr-10" icon={icon} size={25}/>
+      </span>
+    </div>
+  );
+}
+
+export default function Login() {
+  const [password, setPassword] = useState("");
   return (
     <div className="h-screen flex flex-col">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-indigo-200 w-3/4 p-4 rounded-lg">
         <h1 className="text-xl mb-2">Login</h1>
         <form className="flex flex-col mb-2">
           <input className="w-full rounded-lg p-1 mb-2" type="text" name="username" placeholder="username"/>
-          <div className="flex mb-2">
-            <input className="w-full rounded-lg p-1" type={type} name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <span className="flex justify-around items-center" onClick={handleToggle}>
-              <Icon className="absolute mr-10" icon={icon} size={25}/>
-            </span>
-          </div>
+          <Password password={password} setPassword={setPassword} name="password"/>
           <button className="w-full bg-emerald-500 rounded-lg"><span className="p-4 py-2 text-white">login</span></button>
         </form>
         <p><a href="/signup">Forgot your password?</a></p>
--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,14 +1,25 @@
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye';
+import { useState } from 'react';
+
+import { Password } from './login'
+
 export default function Signup() {
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
   return (
-    <>
-      <h1> Sign-up </h1>
-      <form>
-        <input type="text" name="artist" placeholder="artist name"/><br/>
-        <input type="text" name="email" placeholder="e-mail address"/><br/>
-        <input type="password" name="password" placeholder="password"/><br/>
-        <input type="password" name="confirm-password" placeholder="confirm password"/><br/>
-        <input type="submit"/>
-      </form>
-    </>
+    <div className="h-screen flex flex-col">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-indigo-200 w-3/4 p-4 rounded-lg">
+        <h1 className="text-xl mb-2">Sign Up</h1>
+        <form>
+          <input className="w-full rounded-lg p-1 mb-2" type="text" name="artist" placeholder="artist name"/>
+          <input className="w-full rounded-lg p-1 mb-2" type="text" name="email" placeholder="e-mail address"/>
+          <Password password={password} setPassword={setPassword} name="password"/>
+          <Password password={cPassword} setPassword={setCPassword} name="confirm-password"/>
+          <button className="w-full bg-emerald-500 rounded-lg"><span className="p-4 py-2 text-white">submit</span></button>
+        </form>
+      </div>
+    </div>
   );
 }
