389e666970ace6f602ab4f6dbceeb5e343a59448
Emmett McDow <emmett.mcdow@gmail.com>
2024-09-29 18:58:20 -0700

    Posting new track (still need auth)

--- frontend/src/routes/artist.tsx
+++ frontend/src/routes/artist.tsx
@@ -2,7 +2,9 @@ import { SongInfo } from './track';
 import {Icon} from 'react-icons-kit';
 import {x} from 'react-icons-kit/feather/x';
 import { useState } from 'react';
+
 import placeholderSquare from "../placeholder-square.png";
+import { iconForService } from "../util"
 
 // TODO: lets get rid of 'h{n}' tags
 // TODO: wait does that hurt accessibility
@@ -19,7 +21,7 @@ function ServiceSelectorBar({selected, setSelected}: {selected: any, setSelected
           newSelected[provider] = true;
           setSelected(newSelected);
         }}>
-          <img className="w-8" alt={alt} src={placeholderSquare} />
+          <img className="w-8" alt={alt} src={iconForService(provider)} />
         </button>
       );
     }
@@ -36,13 +38,13 @@ function ServiceSelectorBar({selected, setSelected}: {selected: any, setSelected
     </div>
   );
 }
-function ServiceURLs({selected, setSelected}: {selected: any, setSelected: Function}) { 
+function ServiceURLs({formData, setFormData, selected, setSelected}: {formData: any, setFormData: any, selected: any, setSelected: Function}) { 
   let serviceURLs = [];
   for (const [provider, state] of Object.entries(selected)) {
     if (state) {    
       serviceURLs.push(
         <div className="flex mb-2" key={provider}>
-          <input className="w-full rounded-lg p-1" type="text" name={provider}  placeholder={provider.charAt(0).toUpperCase() + provider.slice(1) + " URL"}/>
+          <input className="w-full rounded-lg p-1" type="text" name={provider}  value={formData[provider]} onChange={setFormData} placeholder={provider.charAt(0).toUpperCase() + provider.slice(1) + " URL"}/>
           <span className="flex justify-around items-center" onClick={(_) => {
             const newSelected: any = {...selected};
             newSelected[provider] = false;
@@ -67,25 +69,98 @@ function Editor({changeMode}: {changeMode: Function}) {
   const initialState: any = {
     "apple": false,
     "spotify": false,
-    "youtube": false
+    "youtube": false,
+    "tidal": false,
+    "amazon": false,
+    "bandcamp": false
   }
+  const [formData, setFormData] = useState({
+    'message': '',
+    'name': '',
+    'links': {
+      'apple': '',
+      "spotify": '',
+      "youtube": '',
+      "tidal": '',
+      "amazon": '',
+      "bandcamp": ''
+    }
+  });
   const [selected, setSelected] = useState(initialState);
+  const [message, setMessage] = useState("");
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name == "name" || name == "message") {
+      setFormData(prevState => ({
+        ...prevState,
+        [name]: value,
+      }));
+    } else {
+      setFormData(prevState => ({
+        ...prevState,
+        "links": {
+          ...prevState["links"],
+          [name]: value,
+        }
+      }));
+    }
+  };
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, setMessage: Function) => {
+    event.preventDefault();
+
+    // Convert form data to JSON
+    
+    const jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+
+    try {
+      // TODO: switch to https
+      // TODO: switch away from localhost
+      const response = await fetch('http://127.0.0.1:81/track/honestus/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonData
+      });
+
+      if (response.ok) {
+        // TODO: show this to users better
+        console.log('Form submitted successfully');
+        changeMode(Mode.Standby);
+        // window.location.href = "/artist"
+      } else {
+        console.error('Form submission failed');
+        // TODO: better message, highlight problem
+        setMessage("Uh oh, failed to submit: " + response.body)
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle network or other errors
+      setMessage("Uh oh, failed to submit: " + error)
+    }
+  };
+
+  let renderedMessage = (<></>)
+  if (message != "") {
+    renderedMessage = (
+      <div>{message} </div>
+    );
+  }
+
   return (
     <div className="flex flex-col w-3/4 p-4 rounded-lg mx-auto">
-      <form>
+      {renderedMessage}
+      <form onSubmit={(e) => handleSubmit(e, setMessage)}>
         <ServiceSelectorBar selected={selected} setSelected={setSelected}/>
-        <ServiceURLs selected={selected} setSelected={setSelected}/>
+        <ServiceURLs formData={formData} setFormData={handleChange} selected={selected} setSelected={setSelected}/>
         <textarea  className="w-full rounded-lg p-1 mb-2" name="message" placeholder="A message to your fans"/>
         <button className="text-xl w-full bg-rose-500 rounded-lg" onClick={(_) => {
           changeMode(Mode.Standby);
         }}>
           <span className="p-4 py-2 text-white">cancel</span>
         </button>
-        <button className="text-xl w-full bg-emerald-500 rounded-lg" onClick={(_) => {
-          changeMode(Mode.Standby);
-        }}>
-          <span className="p-4 py-2 text-white">drop</span>
-        </button>
+        <input className="text-xl w-full bg-emerald-500 rounded-lg text-white" type="submit"/>
       </form>
     </div>
   );
