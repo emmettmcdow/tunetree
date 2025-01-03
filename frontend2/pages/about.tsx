7cd228bfdedca0df021edfd0dc65b085392a8a15
Emmett McDow <emmett.mcdow@gmail.com>
2025-01-03 01:56:23 -0800

    Unfuglify about

--- frontend2/pages/about.tsx
+++ frontend2/pages/about.tsx
@@ -5,8 +5,9 @@ import Link from 'next/link';
 import Talk from "@/components/talk";
 import { useRef, useState } from "react";
 import UIButton from "@/components/uibutton";
+import { Header } from "./login";
 
-function Panel({statement, detail, img, n}: {statement: string, detail: string, img: string, n: number}) {
+function DynamicPanel({statement, detail, img, n}: {statement: string, detail: string, img: string, n: number}) {
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const [hide, setHide] = useState<boolean>(true);
@@ -33,17 +34,33 @@ function Panel({statement, detail, img, n}: {statement: string, detail: string, 
     <div className="text-xl md:text-2xl flex flex-col items-center my-4">
       <div>
         <span className="mr-2" onClick={handleExpand}>
-          {hide ? <FiChevronRight style={styleWithDelay} className="inline rainbow-svg border-2 rounded cursor-pointer"/> : <FiChevronDown style={styleWithDelay} className="inline rainbow-svg border-2 rounded cursor-pointer"/>}
+          {hide ? <FiChevronRight style={styleWithDelay} className="inline rainbow-svg border-2 rounded-xl cursor-pointer"/> : <FiChevronDown style={styleWithDelay} className="inline rainbow-svg border-2 rounded cursor-pointer"/>}
         </span>
         <span className="inline">{statement}</span>
       </div>
-      <div className={"my-2 fg-color rounded-xl pt-0 text-center w-3/5 " + (hide ? "hidden" : "")}>
-        <video ref={videoRef} className="border-2 rounded-2xl "  autoPlay muted loop playsInline>
+      <div className={"my-2 fg-color rounded-xl pt-0 text-center flex flex-col items-center " + (hide ? "hidden" : "")}>
+        <video ref={videoRef} className="border-2 rounded-2xl w-3/5 "  autoPlay muted loop playsInline>
           <source src={img}/>    
         </video>
-        <span className="text-center m-2 inline-block">{detail}</span>
+        <span className="text-center m-2">{detail}</span>
       </div>
     </div>
+  );
+}
+
+function Panel({statement, detail, img, n}: {statement: string, detail: string, img: string, n: number}) {
+
+  const styleWithDelay = {
+    "--delay":  `${-n}s`
+  } as React.CSSProperties;
+
+  return (
+      <div className={"flex justify-center items-center my-4 fg-color rounded-xl"}>
+        <video className="border-2 rounded-2xl "  width={200} height={200} autoPlay muted loop playsInline>
+          <source src={img}/>    
+        </video>
+        <div className="text-center w-2/5 text-md">{detail}</div>
+      </div>
   );
 }
 
@@ -54,17 +71,22 @@ export default function About() {
     </span>
   );
   return (
-    <div className="min-h-dvh flex flex-col justify-start items-center w-full py-8">
-      <Talk words={words}/>
-      <div className="mt-8 mx-auto">
-        <Panel statement="tunetree grows artists." detail="get a link to your music you can easily share on social media." img="/videos/link.mp4" n={3}/>
-        <Panel statement="tunetree helps artists update fans." detail="we will send your fans an email when you release." img="/videos/email.mp4" n={2}/>
-        <Panel statement="tunetree is place for self expression." detail="pick from a variety of 3D animations which integrate your album art uniquely." img="/videos/art.mp4" n={1}/>
-      </div>
-      <div className="mx-auto my-4 fg-color rounded-xl">
-        <div className="text-2xl text-center mt-4"> get started now </div>
-        <Link href="/login"><UIButton type="neutral" content="login" handle={() => {}} submit={false}/></Link>
-        <Link href="/signup"><UIButton type="neutral" content="signup" handle={() => {}} submit={false}/></Link>
+    <div className="min-h-dvh flex flex-col justify-center items-center w-full py-8">
+      {/*<Talk words={words}/>*/}
+      <div className="flex flex-col justify-between fg-color rounded-xl p-4 w-1/2">
+        <Header left={"what is tunetree?"}/>
+        <div className="my-4">
+          <Panel statement="tunetree grows artists." detail="get a link to your music you can easily share on social media." img="/videos/link.mp4" n={3}/>
+          <Panel statement="tunetree helps artists update fans." detail="we will send your fans an email when you release." img="/videos/email.mp4" n={2}/>
+          <Panel statement="tunetree is place for self expression." detail="pick from a variety of 3D animations which integrate your album art uniquely." img="/videos/art.mp4" n={1}/>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="text-2xl text-center"> get started now </div>
+          <div >
+            <Link href="/login"><UIButton type="neutral" content="login" handle={() => {}} submit={false}/></Link>
+            <Link href="/signup"><UIButton type="neutral" content="signup" handle={() => {}} submit={false}/></Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend2/pages/help.tsx
+++ frontend2/pages/help.tsx
@@ -1,24 +1,28 @@
 import LightButton from "@/components/lightbutton";
 import Talk from "@/components/talk";
+import { Header } from "./login";
 
 export default function Help() {
-  const words = (
-    <span className="text-3xl text-center absolute top-20 left-24 -rotate-12">    
-      help!
-    </span>
-  );
+  // const words = (
+  //   <span className="text-3xl text-center absolute top-20 left-24 -rotate-12">    
+  //     help!
+  //   </span>
+  // );
   return (  
     <div className="min-h-dvh flex flex-col justify-start items-center w-full py-8">
-      <Talk words={words}/>
+      {/*<Talk words={words}/>*/}
       <div className="fg-color rounded-xl p-4">
-        if you are having issues including but not limited to:
-        <ul className="text-white">
-          <li>* forgot password / locked out of account</li>
-          <li>* someone else is using my identify</li>
-          <li>* something is broken</li>
-        </ul>
-        <span className="mr-1"> shoot me a dm on X: </span>
-        <LightButton link="https://x.com/mcd0w/" content="@mcd0w"/>
+        <Header left="help!"/>
+        <div className="mt-2">
+          if you are having issues including but not limited to:
+          <ul className="text-white">
+            <li>* forgot password / locked out of account</li>
+            <li>* someone else is using my identify</li>
+            <li>* something is broken</li>
+          </ul>
+          <span className="mr-1"> shoot me a dm on X: </span>
+          <LightButton link="https://x.com/mcd0w/" content="@mcd0w"/>
+        </div>
       </div>
     </div>
   );
